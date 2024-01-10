dc3d1635c7fbce3619d87f0e654005b131bc3806
Ekaterina Pykhtunova <eegorova12@ya.ru>
2024-01-10 12:56:12 +0300

    Added tm structure.

---
+++ src/include/footer.php
@@ -0,0 +1,13 @@
+</td>
+        </tr>
+
+        <tr>
+            <td align="center" height="40px;">
+               eegorova12@ya.ru 
+            </td>
+        </tr>
+
+    </table>
+        
+    </body>
+</html>
---
+++ src/index.php
@@ -0,0 +1,7 @@
+<?php include './include/header.php'; ?>
+                
+                <h2>MAIN</h2>
+
+<?php include './include/footer.php'; ?>
+
+
