d98e02fdc968564ab6bf7d92d4d56e722c024083
Ekaterina Pykhtunova <eegorova12@ya.ru>
2024-01-30 10:44:25 +0300

    Added selection projects and tasks from database.

--- src/config/settings.php
+++ src/config/settings.php
@@ -3,7 +3,7 @@
 # SETTINGS MYSQL 
 define("MYSQL_HOST", "localhost");
 define("MYSQL_PORT", "3306");
-define("MYSQL_DATABASE", "tm2");
+define("MYSQL_DATABASE", "tm");
 define("MYSQL_USERNAME", "root");
 define("MYSQL_PASSWORD", "12021992");
 
