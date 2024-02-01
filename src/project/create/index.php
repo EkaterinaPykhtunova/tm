b23611ce72d5558f0c819fd6f708a5478f7a524f
Ekaterina Pykhtunova <eegorova12@ya.ru>
2024-02-01 12:43:11 +0300

    Added delete project.

---
+++ src/project/create/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+include '../../config/settings.php';
+include '../../include/database.php';
+
+$name = 'New Project';
+$description = '';
+
+$stmt = mysqli_prepare($connection, "INSERT INTO `tm`.`tm_project` (`name`,`description`) 
+    VALUES (?,?); ");
+
+mysqli_stmt_bind_param($stmt, "ss", $name, $description);
+mysqli_stmt_execute($stmt);
+
+header('location: /projects');
