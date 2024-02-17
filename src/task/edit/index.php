865b198352ce1d56ce29544550ed3a2ea9f98e44
Ekaterina Pykhtunova <eegorova12@ya.ru>
2024-02-17 15:53:32 +0300

    Fix Task/edit.

--- src/task/edit/index.php
+++ src/task/edit/index.php
@@ -11,32 +11,32 @@
 
  ?>
 
-<script>
+ <script>
 
-function submitMainForm() {
-    hideError();
-    const mainForm = document.getElementById('mainForm');
-    
-    const inputName = document.getElementById('inputName');
-    const valueName = inputName.value;
+    function submitMainForm() {
+        hideError();
+        const mainForm = document.getElementById('mainForm');
+        
+        const inputName = document.getElementById('inputName');
+        const valueName = inputName.value;
 
-    if (valueName === '') {
-        showError('Error! Name value is empty...');
-        return;
+        if (valueName === '') {
+            showError('Error! Name value is empty...');
+            return;
+        }
+        
+        const inputDescription = document.getElementById('inputDescription');
+        const valueDescription = inputDescription.value;
+
+        if (valueDescription === '') {
+            showError('Error! Description value is empty...');
+            return;
+        }
+
+        mainForm.submit();
     }
-    
-    const inputDescription = document.getElementById('inputDescription');
-    const valueDescription = inputDescription.value;
 
-    if (valueDescription === '') {
-        showError('Error! Description value is empty...');
-        return;
-    }
-
-    mainForm.submit();
-}
-
-</script>
+    </script>
 
 <h2>EDIT TASK</h2>
 
@@ -46,13 +46,13 @@ function submitMainForm() {
 
     <p>
         <div>NAME</div>
-        <div><input id="inputName" name="name" type="text" style="width: 300px;" value="<?php echo $name; ?>" /></div>
+        <div><input id="inputName" name="name" type="text" style="width: 300px;" value="<?php echo $task->name; ?>" /></div>
     </p>
 
     <p>
         <div>DESCRIPTION</div>
         <div>
-            <textarea id="inputDescription" name="description" style="width: 300px;"><?php echo $description; ?></textarea>
+            <textarea id="inputDescription" name="description" style="width: 300px;"><?php echo $task->description; ?></textarea>
         </div>
     </p>
 
